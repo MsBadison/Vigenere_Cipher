c669167aea49fa6764881f26302fe74963945a5c
Madison Maguire <96500387+MsBadison@users.noreply.github.com>
2024-02-20 13:30:12 -0500

    Finished menu

--- Main.java
+++ Main.java
@@ -3,17 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("1. Create cipher");
         System.out.println("2. Decode cipher");
         System.out.println("3. Quit");
         System.out.print("> ");
 
-        String menuChoice = scanner.nextLine();
+        int menuChoice = scanner.nextInt();
 
-
+        switch (menuChoice) {
+            case 1:
+                encodeMenuOption();
+                break;
+            case 2:
+                decodeMenuOption();
+                break;
+            case 3:
+                System.exit(0);
+        }
     }
+
+    /**
+     * Loops over the plaintext, converting it to ciphertext using the supplied key
+     * @param plainText the text to be encrypted
+     * @param key the encryption key
+     * @return the encoded cipher text
+     */
     public static String encode (String plainText, String key) {
-        String cipher = "";
+        String cipherText = "";
+
+        // Expand the key, so it is longer than the plain text
+        while (key.length() < plainText.length()) {
+            key = key.concat(key);
+        }
+
         for (int i = 0; i < plainText.length(); i++) {
             int plainTextNum = ((int) plainText.charAt(i) - 64);
             int keyNum = ((int) key.charAt(i)) - 65;
@@ -21,13 +44,25 @@ public class Main {
             if (cipherNum < 65) {
                 cipherNum = cipherNum + 26;
             }
-            cipher = cipher.concat(String.valueOf(Character.toChars(cipherNum)));
+            cipherText = cipherText.concat(String.valueOf(Character.toChars(cipherNum)));
         }
-        return cipher;
+        return cipherText;
     }
 
+    /**
+     * Loops over the cipher text, converting it to plaintext using the supplied key
+     * @param cipherText the text to be decrypted
+     * @param key the encryption key
+     * @return the decoded plaintext
+     */
     public static String decode (String cipherText, String key) {
         String plainText = "";
+
+        // Expand the key, so it is longer than the cipher text
+        while (key.length() < cipherText.length()) {
+            key = key.concat(key);
+        }
+
         for (int i = 0; i < cipherText.length(); i++) {
             int cipherTextNum = ((int) cipherText.charAt(i) + 65);
             int keyNum = ((int) key.charAt(i) + 65);
@@ -38,5 +73,34 @@ public class Main {
             plainText = plainText.concat(String.valueOf(Character.toChars(plainTextNum)));
         }
         return plainText;
+    }
+
+    /**
+     * Gets user input and invokes encode function
+     */
+    public static void encodeMenuOption () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the text to be encoded (only letters): ");
+        String plaintext = scanner.nextLine().toUpperCase().replaceAll("[^a-zA-Z]", "");
+
+        System.out.print("Enter the encryption key (only letters): ");
+        String key = scanner.nextLine().toUpperCase().replaceAll("[^a-zA-Z]", "");
+
+        String cipherText = encode(plaintext, key);
+        System.out.print("The encrypted text is: " + cipherText);
+    }
+
+    /**
+     * Gets user input and invokes decode function
+     */
+    public static void decodeMenuOption () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the text to be decoded (only letters): ");
+        String ciphertext = scanner.nextLine().toUpperCase().replaceAll("[^a-zA-Z]", "");
+        System.out.print("Enter the encryption key (only letters): ");
+        String key = scanner.nextLine().toUpperCase().replaceAll("[^a-zA-Z]", "");
+        String plaintext = decode(ciphertext, key);
+        System.out.print("The decrypted text is: " + plaintext);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Vigenere_Cipher
+
+This is a simple console application that can encode/decode a string using the Vigenere Cipher and a given key. 
+
+If there are any spaces or non-alphabetic characters in the plain text, these will be removed.
+
+The plain text and key will be converted to uppercase. 
+
+If the key is shorter than the plain text, the key will be looped. 
